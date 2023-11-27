49bd2770cc573f33aea4458124401b8a373e7bc6
Jaztin <77910637+csun-jtabu@users.noreply.github.com>
2023-11-26 21:31:44 -0800

    fixed phase 3 part 9 sql query
    fixed phase 3 part 9 query

--- COMP-440-ProjectWorkSpace/sql-files-phase-3/phase3_part9.sql
+++ COMP-440-ProjectWorkSpace/sql-files-phase-3/phase3_part9.sql
@@ -1,14 +1,19 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `phase3_part9`()
 BEGIN
-	(SELECT *
-	FROM user
-	WHERE username NOT IN
-		(SELECT user_inserted
-		FROM item
-		WHERE productid IN
-			(SELECT productid
-			FROM review
-			WHERE rating = 'poor')
-		)
+	(SELECT t1.username, t1.password, t1.firstName, t1.lastName, t1.email
+	FROM
+		(SELECT u.username, u.password, u.firstName, u.lastName, u.email
+		FROM user u INNER JOIN item i
+		ON u.username = i.user_inserted
+		INNER JOIN review r
+		ON i.productid = r.productid
+		WHERE r.rating != 'poor') as t1
+	WHERE t1.username NOT IN
+			(SELECT u2.username
+			FROM user u2 INNER JOIN item i2
+			ON u2.username = i2.user_inserted
+			INNER JOIN review r2
+			ON i2.productid = r2.productid
+			WHERE r2.rating = 'poor') 
 	);
 END
